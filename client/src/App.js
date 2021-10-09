08036fce20d7744a1f4260806a33745a2f8540e9
Bigjames728 <bigjames728@gmail.com>
2021-10-09 16:40:20 -0700

    Added Login component back in

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import ExercisesList from './components/exercises-list';
 import EditExercise from './components/edit-exercise';
 import CreateExercise from './components/create-exercise';
 import CreateUser from './components/create-user';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className="container">
         <NavBar />
           <br/>
+            <Route path="/login" exact component={Login} />
             <Route path="/" exact component={ExercisesList} />
             <Route path="/edit/:id" exact component={EditExercise} />
             <Route path="/create" exact component={CreateExercise} />
