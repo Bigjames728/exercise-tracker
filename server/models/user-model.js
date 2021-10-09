31daedfe82a4dc6db24826e8b3b365b5a3a3be28
Bigjames728 <bigjames728@gmail.com>
2021-10-08 18:21:50 -0700

    Added password hashing to the user model with a pre method

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -24,6 +24,24 @@ const userSchema = new Schema({
     timestamps: true
 });
 
+// Mongoose provides something called a pre-save hook. It's a function that mongoose runs just before saving a record to mongoDB.
+// In this case, I want to hash a password just before it is stored in my database. I do this by calling the pre method on my schema.
+// This method takes two arguments, the hook name and a function.
+
+userSchema.pre('save', function(next) {
+    var user = this; // this is assigned to the user object and all of its data. So now "user" holds all this data.
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
+
+
+
